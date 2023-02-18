ec38ab188109bd9a11efd1cdfd882fc4c6706845
dermg13 <dermg13@gmail.com>
2023-02-18 12:05:03 -0500

    added others page and test class

---
+++ src/test/java/pages/OthersPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class OthersPage extends BasePage{
+    public OthersPage(WebDriver driver) { PageFactory.initElements(driver,this); }
+
+    @FindBy(id="btn-status")
+    public WebElement statusBtn;
+
+    @FindBy(id="toggle-btn")
+    public WebElement toggleBtn;
+}
---
+++ src/test/java/tests/OthersTest.java
@@ -0,0 +1,35 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.InputsPage;
+import pages.OthersPage;
+
+public class OthersTest extends BaseTest{
+
+    OthersPage page;
+
+    @BeforeMethod
+    public void setUp(){
+        page = new OthersPage(driver);
+        driver.get("http://automation.techleadacademy.io/#/others");
+    }
+
+    @Test(testName = "US2001 - Disabled button", description = "“Disabled Button” on Others page is disabled initially")
+    public void test01(){
+        String disableText = "DISABLED BUTTON";
+        Assert.assertEquals(page.statusBtn.getText(),disableText);
+    }
+
+    @Test(testName = "US2002 - Enabled option from toggle button", description = "I need a Toggle button that will enable disabled button on Others page")
+    public void test02(){
+        String enableText = "ENABLED BUTTON";
+        page.toggleBtn.click();
+
+        Assert.assertEquals(page.statusBtn.getText(),enableText);
+    }
+
+
+
+}
